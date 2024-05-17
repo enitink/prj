0100b7488d40f7e24e1a86748c27b37c10926656
Nitin Kumar <enitin.yk@gmail.com>
2024-05-17 22:55:21 +0530

    leet code

---
+++ generic/codes/leetcode/go/1325-deletenodes.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = removeLeafNodes(root.Left, target)
+	root.Right = removeLeafNodes(root.Right, target)
+
+	if root.Val == target && root.Left == nil && root.Right == nil {
+		return nil
+	}
+	return root
+}
